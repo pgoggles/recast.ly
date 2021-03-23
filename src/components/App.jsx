f5f59100838fe011581b473378aede790536562e
Anindya Mehta <anindyamehta@gmail.com>
2021-03-22 20:35:12 -0400

    Refactor App to ES6 format

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,34 @@
 import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
-import exampleVideoData from '../data/exampleVideoData.js';
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> <Search /> </h5></div>
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> <Search /> </h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em> <VideoPlayer video={this.props.exampleVideoData[0]}/> </h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> <VideoList videos={this.props.exampleVideoData}/> </h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> <VideoPlayer video={exampleVideoData[0]}/> </h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> <VideoList videos={exampleVideoData}/> </h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+}
 
 export default App;
