c97372253d4dee9014ca7dffda39aa5e7395fcc3
Anindya Mehta <anindyamehta@gmail.com>
2021-03-22 20:07:16 -0400

    Implement a videoList, a videoPlayer and videoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,16 +11,13 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> <VideoPlayer /> </h5></div>
+        <div><h5><em>videoPlayer</em> <VideoPlayer video={exampleVideoData[0]}/> </h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> <VideoList /> </h5></div>
+        <div><h5><em>videoList</em> <VideoList videos={exampleVideoData}/> </h5></div>
       </div>
     </div>
   </div>
 );
 
-
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
 export default App;
